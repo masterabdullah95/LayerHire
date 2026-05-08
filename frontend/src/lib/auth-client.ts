801295cd1a9259f166b1ba19e9806724288591f8
Abdullah Shoaib <masterabdullah95@gmail.com>
2026-05-08 18:07:36 +0500

    cookies bug fixing for production

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -3,11 +3,11 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   // baseURL: import.meta.env.VITE_API_URL,
-  // baseURL: import.meta.env.VITE_APP_URL, // For reverse proxy, we are using frontend url, otherwise we have to put backend api url
-  
+  baseURL: import.meta.env.VITE_APP_URL, // For reverse proxy, we are using frontend url, otherwise we have to put backend api url
+
   // If your proxy handles everything under /api, 
   // you can often use a relative path.
-  baseURL: "/api",
+  // baseURL: "/api",
   fetchOptions: {
     credentials: 'include',           // sends cookies
   },
