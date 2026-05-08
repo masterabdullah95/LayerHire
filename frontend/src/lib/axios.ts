e11b2640748ddf308b59dcbd9d19fa97660b3511
Abdullah Shoaib <masterabdullah95@gmail.com>
2026-05-08 11:25:13 +0500

    add some host variables for production

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_URL +'api',
-  baseURL: import.meta.env.VITE_APP_URL +'api',
+  baseURL: import.meta.env.VITE_APP_URL +'/api',
   withCredentials: true,           // sends cookies with every request
   headers: {
     'Content-Type': 'application/json',
