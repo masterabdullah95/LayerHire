d5d4f05ec7730dc646d3f2f628ad1565ba9a8c30
Abdullah Shoaib <masterabdullah95@gmail.com>
2026-05-08 11:16:46 +0500

    add some host variables for production

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -2,7 +2,8 @@ import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_APP_URL, // For reverse proxy, we are using frontend url, otherwise we have to put backend api url
   fetchOptions: {
     credentials: 'include',           // sends cookies
   },
